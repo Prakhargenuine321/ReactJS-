8c222aacbc9fbba4983b2cbec975e292eee6c164
Prakhar <prakhargenuine9336@gmail.com>
2024-08-25 13:46:07 +0530

    Learning difference between react app and react+vite app

---
+++ 00onlyreact/src/Chai.js
@@ -0,0 +1,15 @@
+//same thing we did it in ".jsx"(vite) but we need to also do here 
+//the difference is often that here we can use ".js" file also 
+//as we are not able to do it in "vite" app.
+
+
+//function name should be in Uppercase
+//best practice is to file-name should also be in Uppercase.
+
+function Chai(){
+    return(
+        <h2>Chai in react</h2>
+    );
+}
+
+export default Chai
